e8636ce03815981c54f61f9cd8549424d853dcc4
Georgios Tselikas <georgios.p.tselikas@gmail.com>
2024-12-10 20:53:08 +0200

    test

--- gr/aueb/cf/ch1/HelloWorld.java
+++ gr/aueb/cf/ch1/HelloWorld.java
@@ -6,5 +6,6 @@ package gr.aueb.cf.ch1;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello Coding Factory!");
+        System.out.println("Good night");
     }
 }
